ea38481a22c0d0ee383c4a5afdf605a5d4c48694
Dima Boiko <boikod887@gmail.com>
2024-01-12 16:59:33 +0200

    loader fix

--- app/config/api.js
+++ app/config/api.js
@@ -6,7 +6,7 @@ export const discordRedirectLink = 'https://discord.com/api/oauth2/authorize?cli
 // export const discordRedirectLink = 'https://discord.com/api/oauth2/authorize?client_id=1082648354053427210&redirect_uri=https%3A%2F%2Fnoname-backend-production.up.railway.app%2Fdiscord&response_type=code&scope=identify'
 // export const discordRedirectLink = 'https://discord.com/api/oauth2/authorize?client_id=1082648354053427210&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fdiscord&response_type=code&scope=identify'
 
-const api = 'https://noname-backend-dev-production.up.railway.app'
+export const api = 'https://noname-backend-dev-production.up.railway.app'
 // const api = 'https://noname-backend-production.up.railway.app'
 // export const api = 'http://localhost:5000'
 
